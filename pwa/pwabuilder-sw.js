d6aa4b8aa04369d3790c51960df33e0853c86f15
LIGMATV <danishnaufal049@gmail.com>
2024-02-20 08:23:56 +0700

    Update pwabuilder-sw.js

--- pwa/pwabuilder-sw.js
+++ pwa/pwabuilder-sw.js
@@ -1,14 +1,28 @@
-// This is the "Offline copy of pages" service worker
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-const CACHE = "pwabuilder-offline";
+const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
 workbox.routing.registerRoute(
   new RegExp('/*'),
@@ -17,38 +31,24 @@ workbox.routing.registerRoute(
   })
 );
 
-// Select files for caching.
-let urlsToCache = [
-  "/",
-  "/index.html",
-  "/logo/favicon.svg",
-  "/font",
-  "/logo",
-  "/pwa",
-  "/pwa/android",
-  "/pwa/ios",
-  "/pwa/windows11", 
-  "/pwa/pwa-handler.js",
-];
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
 
-// Cache all the selected items once application is installed.
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-      caches.open(CACHE).then((cache) => {
-          console.log("Caching started.");
-          return cache.addAll(urlsToCache);
-      })
-  );
-});
+        if (preloadResp) {
+          return preloadResp;
+        }
 
-// Whenever a resource is requested, return if its cached else fetch the resourcefrom server.
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-      caches.match(event.request).then((response) => {
-          if (response) {
-              return response;
-          }
-          return fetch(event.request);
-      })
-  );
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
