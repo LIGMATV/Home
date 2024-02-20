7b107cdde189325fd27ae3e3ee88869717871c1c
LIGMATV <danishnaufal049@gmail.com>
2024-02-20 08:22:32 +0700

    Update

--- pwa/pwabuilder-sw.js
+++ pwa/pwabuilder-sw.js
@@ -16,3 +16,39 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 );
+
+// Select files for caching.
+let urlsToCache = [
+  "/",
+  "/index.html",
+  "/logo/favicon.svg",
+  "/font",
+  "/logo",
+  "/pwa",
+  "/pwa/android",
+  "/pwa/ios",
+  "/pwa/windows11", 
+  "/pwa/pwa-handler.js",
+];
+
+// Cache all the selected items once application is installed.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+      caches.open(CACHE).then((cache) => {
+          console.log("Caching started.");
+          return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Whenever a resource is requested, return if its cached else fetch the resourcefrom server.
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+      caches.match(event.request).then((response) => {
+          if (response) {
+              return response;
+          }
+          return fetch(event.request);
+      })
+  );
+});
